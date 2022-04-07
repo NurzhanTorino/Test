315f1dc947ebdd55b975cc9a8310e3a6237b7cfc
NurzhanTorino <nurzhan.torino@mail.ru>
2022-04-07 11:01:07 +0600

    first commit

---
+++ script/script.js
@@ -0,0 +1,68 @@
+
+//sun
+let modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn")
+let span = document.getElementsByClassName("close")[0]
+span.onclick = function(){
+    modal.style.display = "none";
+}
+btn.onclick = function(){
+    modal.style.display = "block";
+}
+
+
+// earth
+let modall = document.getElementById('myModale');
+var btnn = document.getElementById("xaxa")
+let spann = document.getElementsByClassName("closee")[0]
+spann.onclick = function(){
+    modall.style.display = "none";
+}
+btnn.onclick = function(){
+    modall.style.display = "block";
+}
+
+//jupiter
+
+let container = document.getElementById("main")
+let planet = document.getElementById("myBtnJupiter")
+let zak = document.getElementsByClassName("zakryt")[0]
+
+zak.onclick = function(){
+    container.style.display = "none";
+
+}
+planet.onclick = function(){
+    container.style.display = "block"
+}
+
+//Saturn
+
+
+let block = document.getElementById("blockk")
+let planeta = document.getElementById("myBtnSaturn")
+let jok = document.getElementById("byeBye")
+
+planeta.onclick = function(){
+    block.style.display = "block"
+}
+
+jok.onclick = function(){
+    block.style.display = "none"
+}
+
+
+
+//Uran
+/*
+let abc = document.getElementById("allsight")
+let deff = document.getElementById("myBtnUran")
+let ghl = document.getElementsByClassName("displayNone")[0]
+
+ghl.onclick = function(){
+    abc.style.display = "none";
+}
+deff.onclick = function(){
+    abc.style.display = "block"
+}
+*/
